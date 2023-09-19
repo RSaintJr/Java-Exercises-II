764e3a028a4c492b8ed9cd39e677706acf7826a6
Roberto Junior <robertomep7@gmail.com>
2023-09-19 14:31:15 -0300

    Update ServidorSocket.java

--- Socket_Singleton/src/Socket/ServidorSocket.java
+++ Socket_Singleton/src/Socket/ServidorSocket.java
@@ -1,9 +1,6 @@
 package Socket;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -19,36 +16,44 @@ public class ServidorSocket {
         clientes = new ArrayList<>();
     }
 
-    public static ServidorSocket getInstance(int port) throws IOException {
+    private static synchronized ServidorSocket getInstance(int port) throws IOException {
         if (instance == null) {
             instance = new ServidorSocket(port);
         }
         return instance;
     }
 
-    public void startServer() {
+    private void startServer() {
         while (true) {
             try {
                 Socket cliente = servidor.accept();
-                clientes.add(cliente);
+                synchronized (clientes) {
+                    clientes.add(cliente);
+                }
                 new Thread(() -> {
                     try {
                         BufferedReader input = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
                         String mensagem;
                         while ((mensagem = input.readLine()) != null) {
-                            for (Socket outroCliente : clientes) {
-                                if (outroCliente != cliente) {
-                                    PrintStream saida = new PrintStream(outroCliente.getOutputStream());
-                                    saida.println(mensagem);
+                            synchronized (clientes) {
+                                for (Socket outroCliente : clientes) {
+                                    if (outroCliente != cliente) {
+                                        try {
+                                            PrintStream saida = new PrintStream(outroCliente.getOutputStream());
+                                            saida.println(mensagem);
+                                        } catch (IOException e) {
+                                            System.out.println("Erro ao enviar mensagem: " + e.getMessage());
+                                        }
+                                    }
                                 }
                             }
                         }
                     } catch (IOException e) {
-                        System.out.println("Algo errado aconteceu");
+                        System.out.println("Erro ao aceitar Cliente: " + e.getMessage());
                     }
                 }).start();
             } catch (IOException e) {
-                System.out.println("Algo errado aconteceu");
+                System.out.println("Algo deu errado: " + e.getMessage());
             }
         }
     }
@@ -58,8 +63,7 @@ public class ServidorSocket {
             ServidorSocket servidor = ServidorSocket.getInstance(7000);
             servidor.startServer();
         } catch (IOException e) {
-            System.out.println("Algo errado aconteceu");
+            System.out.println("Erro ao iniciar o servidor: " + e.getMessage());
         }
     }
-
 }
